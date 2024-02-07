39318c8b01a555ab69f7fcd22e0c9b26081af36f
OttoTheBigMan <ottsil@edu.umea.se>
2024-02-07 12:23:26 +0100

    Temp login / register page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,16 @@
 <script lang="ts">
+    import { enhance } from '$app/forms';
     import type { PageData } from './$types';
     
     export let data: PageData;
 </script>
+
+<main>
+    <form action="?/login" method="post" use:enhance>
+        
+        <label>Username: <input type="text" name="username"></label>
+        <label>Password: <input type="password" name="password"></label>
+        <button>Log In</button>
+    </form>
+    <a href="/register">Don't have an account?</a>
+</main>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
+    import { enhance } from '$app/forms';
     import type { PageData } from './$types';
     
     export let data: PageData;
 </script>
+
+<main>
+    <form action="?/login" method="post" use:enhance>
+        
+        <label>Username: <input type="text" name="username"></label>
+        <label>Password: <input type="password" name="password"></label>
+        <label>Repeat Password: <input type="password" name="password"></label>
+        <button>Log In</button>
+    </form>
+    <a href="/login">Already have account?</a>
+</main>
