1e594eb34296b412618212d732bc3563ea3c29d8
OttoTheBigMan <ottsil@edu.umea.se>
2024-02-08 13:11:44 +0100

    Yep

--- src/routes/activities/+page.svelte
+++ src/routes/activities/+page.svelte
@@ -8,7 +8,7 @@
     <h1>Activities</h1>
     <ul>
         {#each data.activities as activity}
-            <li><a href="/activities/{activity.id}">{activity.name}</a></li>
+            <li><a href="/activities/perform/{activity.id}">{activity.name}</a> | {activity.points} Points</li>
         {/each}
     </ul>
     <a href="/activities/suggest">Suggest an activity</a>
---
+++ src/routes/activities/perform/[activity]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<main>
+    <h1>{data.info.name}</h1>
+    <h2>Grants {data.info.points} points</h2>
+    <span>{data.info.description}</span>
+
+    <h1>Perform the activity</h1>
+    <form action="?/perform/{data.info.id}" method="post">
+        <button>Perform</button>
+    </form>
+</main>
