d91248c4a7eebde247fd2a62ac560e97255380ab
OttoTheBigMan <ottsil@edu.umea.se>
2024-02-13 11:08:53 +0100

    You can like shit now 😱

--- src/routes/(logged in)/activities/+page.svelte
+++ src/routes/(logged in)/activities/+page.svelte
@@ -1,14 +1,23 @@
 <script lang="ts">
+    import { enhance } from '$app/forms';
     import type { PageData } from './$types';
     
     export let data: PageData;
+
+    $: activities = data.activities;
 </script>
 
 <main>
     <h1>Activities</h1>
     <ul>
-        {#each data.activities as activity}
-            <li><a href="/activities/perform/{activity.id}">{activity.name}</a> | {activity.points} Points</li>
+        {#each activities as activity}
+            <li><a href="/activities/perform/{activity.id}">{activity.name}</a> | {activity.points} Points
+                <form action="?/like" method="post" use:enhance>
+                    <input type="hidden" name="activityId" value="{activity.id}">
+                    <button><i class="fa-{activity.liked ? "solid" : "regular"} fa-heart"></i></button>
+                </form>
+                | {activity.likeCount} Likes
+            </li>
         {/each}
     </ul>
     <a href="/activities/suggest">Suggest an activity</a>
