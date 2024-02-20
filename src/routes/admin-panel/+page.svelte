9cedf15eaa7850fbe0d0659c49ad14d38327fa25
Anton Sjögren <antsjo03@edu.umea.se>
2024-02-20 10:38:33 +0100

    wasdwadwad

--- src/routes/admin-panel/+page.svelte
+++ src/routes/admin-panel/+page.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
+    import { enhance } from '$app/forms';
     import type { PageData } from './$types';
     
     export let data: PageData;
 </script>
 
+
+{#each data.activities as activity}
+    <div>
+        <h2>{activity.name}</h2>
+        <p>{activity.description}</p>
+    </div>
+    <form action="?/approve" method="post" use:enhance>
+        <input type="hidden" name="activityId" value="{activity.id}">
+        <button>Approve</button>
+    </form>
+    <form action="?/deny" method="post" use:enhance>
+        <input type="hidden" name="activityId" value="{activity.id}">
+        <button>Deny</button>
+    </form>
+{/each}
